19ef2b54f4d3c77cc21d328cb40f4769332ebcfc
Evgenia Popova <jane477@yandex.ru>
2024-08-21 15:48:46 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 13_676; // стоимость билета
+        int y = 20; // количество рублей для одной бонусной мили
+
+        int z = x/y; // количество бонусных миль
+
+        System.out.println("Количество бонусных миль: " + z);
+
+        // Ответ сохраняете в
+        // новую переменную и выводите на экран
+    }
+}
